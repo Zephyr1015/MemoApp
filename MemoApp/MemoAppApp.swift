d57d76e44a3dd1a2965a8fc9793f588ad82e5835
Zephyr1015 <kurihana_1015@icloud.com>
2024-05-04 17:38:27 +0900

    Initial Commit

---
+++ MemoApp/MemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoAppApp.swift
+//  MemoApp
+//
+//  Created by Vincent on 2024/05/04.
+//
+
+import SwiftUI
+
+@main
+struct MemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
